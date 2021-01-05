24b252db7c6a1228c4c8a45daf0bdf71bc7a7aea
fireflower0 <fireflower0@hotmail.co.jp>
2021-01-05 10:29:36 +0900

    Fixed a name

---
+++ 01_Case/04/04.sql
@@ -0,0 +1,31 @@
+CREATE TABLE PopTbl2 (
+  pref_name text,
+  sex integer,
+  population integer
+);
+
+INSERT INTO PopTbl2
+VALUES ('徳島', 1,  60),
+       ('徳島', 2,  40),
+       ('香川', 1, 100),
+       ('香川', 2, 100),
+       ('愛媛', 1, 100),
+       ('愛媛', 2,  50),
+       ('高知', 1, 100),
+       ('高知', 2, 100),
+       ('福岡', 1, 100),
+       ('福岡', 2, 200),
+       ('佐賀', 1,  20),
+       ('佐賀', 2,  80),
+       ('長崎', 1, 125),
+       ('長崎', 2, 125),
+       ('東京', 1, 250),
+       ('東京', 2, 150);
+
+SELECT pref_name,
+       SUM( CASE WHEN sex = 1 THEN population ELSE 0 END ) AS cnt_m,
+       SUM( CASE WHEN sex = 2 THEN population ELSE 0 END ) AS cnt_f
+FROM PopTbl2
+GROUP BY pref_name;
+
+DROP TABLE PopTbl2;
