475ccc64b123505f014187d7d095a8c8c0f2c1c8
fireflower0 <fireflower0@hotmail.co.jp>
2021-01-19 14:54:45 +0900

    Add a 05 in 06_Having

---
+++ 06_Having/05/05.sql
@@ -0,0 +1,31 @@
+CREATE TABLE Students (
+  student_id INTEGER PRIMARY KEY,
+  dpt VARCHAR(16) NOT NULL,
+  sbmt_date DATE
+);
+
+INSERT INTO Students
+VALUES (100, '理学部', '2018-10-10'),
+       (101, '理学部', '2018-09-22'),
+       (102, '文学部', NULL),
+       (103, '文学部', '2018-09-10'),
+       (200, '文学部', '2018-09-22'),
+       (201, '工学部', NULL),
+       (202, '経済学部', '2018-09-25');
+
+-- 提出日にNULLを含まない学部を選択する：その1 COUNT関数の利用
+SELECT dpt
+FROM Students
+GROUP BY dpt
+HAVING COUNT(*) = COUNT(sbmt_date);
+
+-- 提出日にNULLを含まない学部を選択する：その2 CASE式の利用
+SELECT dpt
+FROM Students
+GROUP BY dpt
+HAVING COUNT(*) = SUM(
+  CASE WHEN sbmt_date IS NOT NULL
+       THEN 1 ELSE 0 END
+);
+
+DROP TABLE Students;
